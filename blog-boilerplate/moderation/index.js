15a9a23b11a69e9cdf00727ab35fd3e677da5c89
HungTruong4689 <hungphuc4689@gmail.com>
2023-11-06 18:33:00 +0100

    event query

--- blog-boilerplate/moderation/index.js
+++ blog-boilerplate/moderation/index.js
@@ -5,24 +5,26 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
-app.post('/events', (req, res) => {
-    const event = req.body
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body
 
-    axios.post('http://localhost:4000/events', event).catch((err) => {
-        console.log(err.message)
-    })
-    axios.post('http://localhost:4001/events', event).catch((err) => {
-        console.log(err.message)
-    })
-    axios.post('http://localhost:4002/events', event).catch((err) => {
-        console.log(err.message)
-    })
-    axios.post('http://localhost:4003/events', event).catch((err) => {
-        console.log(err.message)
-    })
-    res.send({ status: 'OK' })
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('orange') ? 'rejected' : 'approved'
+
+        await axios.post('http://localhost:4005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                postId: data.postId,
+                status,
+                content: data.content,
+            },
+        })
+    }
+
+    res.send({})
 })
 
-app.listen(4005, () => {
-    console.log('Listening on 4005')
+app.listen(4003, () => {
+    console.log('Listening on 4003')
 })
